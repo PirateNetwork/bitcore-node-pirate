a7a053888c7909177963dc5961e835a0d1e8ae1c
Cryptoforge <cryptoforge.cc@gmail.com>
2021-11-24 18:23:31 -0800

    Pirate port

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -5,7 +5,7 @@ var bitcoin = require('bitcoin');
 var async = require('async');
 var maxTime = 20;
 
-console.log('Komodo Service native interface vs. Komodo JSON RPC interface');
+console.log('Pirate Service native interface vs. Pirate JSON RPC interface');
 console.log('----------------------------------------------------------------------');
 
 // To run the benchmarks a fully synced Bitcore Core directory is needed. The RPC comands
@@ -28,7 +28,7 @@ var fixtureData = {
 
 var bitcoind = require('../').services.Bitcoin({
   node: {
-    datadir: process.env.HOME + '/.komodo',
+    datadir: process.env.HOME + '/.komodo/PIRATE/',
     network: {
       name: 'testnet'
     }
@@ -43,16 +43,16 @@ bitcoind.start(function(err) {
   if (err) {
     throw err;
   }
-  console.log('Komodo started');
+  console.log('Pirate started');
 });
 
 bitcoind.on('ready', function() {
 
-  console.log('Komodo ready');
+  console.log('Pirate ready');
 
   var client = new bitcoin.Client({
     host: 'localhost',
-    port: 18332,
+    port: 45452,
     user: 'bitcoin',
     pass: 'local321'
   });
